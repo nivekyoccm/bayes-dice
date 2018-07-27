f05fb5295de14a8fb2865f253fa8daed9d2b5b1e
nivekyoccm <34323611+nivekyoccm@users.noreply.github.com>
2018-07-27 15:13:30 -0500

    notes on design plus initial work on class setup

--- src/bayesdice.py
+++ src/bayesdice.py
@@ -1,3 +1,35 @@
-class BayesDice:
-    pass
+import numpy as np
+import pandas as pd
+import random
 
+
+class BayesDice:
+    def __init__(self, _id, sides, expected_avg, roll):
+        self._id = _id
+        self.sides = sides
+        self.expected_avg = expected_avg
+
+    def sides(self):
+        random.choice([4,6,8,12 20])
+
+    def expected_avg(self):
+        nums = []
+        for num in range(1,self.sides+1):
+            nums.append(num)
+        self.sides.mean()
+
+    def roll(self):
+        return random.randrange(1, self.sides + 1)
+
+    def __str__(self):
+        return "Die ID: {} with {} sides".format(self._id, self._sides)
+
+    def __repr__(self):
+        return self.__str__()
+
+
+# bd = BayesDice()
+# bd.choose_die()
+# bd.roll_die()
+# bd.roll_die()
+# bd.roll_die()
---
+++ src/main.py
@@ -0,0 +1,23 @@
+# 
+# choose one of 5 dies randomly by side selection
+# whichever chosen should be instantiated
+# as having # of sides expected as one of:  4, 6, 8, 12, and 20 sided
+# each must have expected avg static
+# should be unknown to program
+# prior est established and bayes implemented
+
+# roll method on die executed with face returned randomly of potentials
+# bayes run based on return val with prior at .2 per potential die ID
+# any above discards those potentials
+# otherwise the average is updated and compared to expected for 5 potentials
+# loop on above until one ID in dict? of over .95
+
+# best is chosen as most likely
+# 
+
+
+# bd = BayesDice()
+# bd.choose_die()
+# bd.roll_die()
+# bd.roll_die()
+# bd.roll_die()
